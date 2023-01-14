5f9cc27a5078a7f2ca56d9174a125e2b3f5bd040
Hve <hve4638@gmail.com>
2023-01-15 08:45:23 +0900

    add detail mode

---
+++ getip/IPTuple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getip
+{
+    internal class IPName
+    {
+        public string? IP { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- getip/Program.cs
+++ getip/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace getip
 {
@@ -7,46 +9,78 @@ namespace getip
     {
         static void Main(string[] args)
         {
-            var output = RunCommand("ipconfig");
-            var ip = parseIP(output);
-
-            Console.WriteLine(ip);
+            bool help = args.Contains("-h") || args.Contains("?");
+            bool simple = args.Contains("-s");
+                
+            if (help)
+            {
+                Console.WriteLine("Option:");
+                Console.WriteLine("  -s\tShow simply");
+            }
+            else Run(simple);
         }
 
-        static string RunCommand(string name, string args="")
+        static void Run(bool simple)
         {
-            // Start the child process.
-            Process p = new Process();
-            // Redirect the output stream of the child process.
+            var lines = RunCommand("ipconfig");
+            var results = ParseIP(lines);
+
+            foreach (var result in results)
+            {
+                if (simple) Console.WriteLine(result.IP);
+                else Console.WriteLine($"{result.IP,-16}{result.Name}");
+            }
+        }
+
+        static List<IPName> ParseIP(string[] lines)
+        {
+            Regex reName = new(
+                @"^(?! )(.+):",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline
+            );
+            Regex reIPv4 = new(
+                @"^.*IPv4.+(\.| )+: *(?'IP'\d+\.\d+\.\d+\.\d+).*$",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline
+            );
+
+            List<IPName> results = new();
+            IPName? ipName = null;
+            foreach (var line in lines)
+            {
+                if (reName.IsMatch(line))
+                {
+                    var match = reName.Match(line);
+                    ipName = new()
+                    {
+                        Name = match.Groups[1].ToString(),
+                    };
+                }
+                else if (ipName != null && reIPv4.IsMatch(line))
+                {
+                    var match = reIPv4.Match(line);
+                    ipName.IP = match.Groups[2].ToString();
+
+                    results.Add(ipName);
+                }
+            }
+
+            return results;
+        }
+
+        static string[] RunCommand(string name, string args="")
+        {
+            Process p = new();
+
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.FileName = name;
             p.StartInfo.Arguments = args;
             p.Start();
-            // Do not wait for the child process to exit before
-            // reading to the end of its redirected stream.
-            // p.WaitForExit();
-            // Read the output stream first and then wait.
+
             string output = p.StandardOutput.ReadToEnd();
             p.WaitForExit();
-            return output;
-        }
 
-        static string parseIP(string text)
-        {
-            string output = "";
-            Regex rx = new Regex(
-                @"^.*IPv4.+(\.| )+: *(?'IP'\d+\.\d+\.\d+\.\d+).*$",
-                RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline
-            );
-
-            MatchCollection matches = rx.Matches(text);
-            foreach (Match match in matches)
-            {
-                output += $"{match.Groups[2]}\n";
-            }
-            
-            return output.Trim();
+            return output.Split('\n');
         }
     }
 }
